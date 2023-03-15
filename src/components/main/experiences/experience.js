41c03ef7379d5025dfe0eea02e1ad7877bc4c9e1
Berat Uçar <ownertr55@gmail.com>
2023-03-15 16:14:51 +0300

    portfolio-v6

--- src/components/main/experiences/experience.js
+++ src/components/main/experiences/experience.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Experience = ({ company, title, date, desc }) => {
+const Experience = ({ company, title, date, desc, location }) => {
   return (
     <div className="top-item resume-item">
       <h2>{company}</h2>
       <span>
-        {title} | {date}
+        {title} | {date} | {location}
       </span>
       <p>{desc}</p>
     </div>
