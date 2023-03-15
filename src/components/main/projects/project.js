02ef171683d762976c0703fc481b3e4a9a244e26
Berat Uçar <ownertr55@gmail.com>
2023-03-15 17:42:50 +0300

    portfolio-v8

--- src/components/main/projects/project.js
+++ src/components/main/projects/project.js
@@ -5,10 +5,12 @@ const Project = ({ name, desc, github, desc2 }) => {
     <>
       <div className="project-info clearfix">
         <h2>
-          <a href="https://beratucar6.netlify.app/">{name}</a>
+          <a href="https://beratucar6.netlify.app/" target={"_blank"}>{name}</a>
         </h2>
         <div>{desc}</div>
-        <a href={github}>{github}</a>
+        <a href={github} target={"_blank"}>
+          {github}
+        </a>
         <div>{desc2}</div>
       </div>
     </>
